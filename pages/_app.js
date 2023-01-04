d81b5a0f115e8e92b22fac715a9c2fb9dec5f284
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2023-01-05 00:55:18 +0530

    ShippingScreen created with authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/router';
 // import type { AppProps } from 'next/app'
 
 export default function App({
@@ -11,8 +12,28 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
+}
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+  return children;
 }
---
+++ pages/unauthorized.js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function Unauthorized() {
+  const router = useRouter();
+  const { message } = router.query;
+
+  return (
+    <Layout title="Unauthorized Page">
+      <div className='mx-auto text-center'>
+        <h1 className="text-2xl">Access Denied</h1>
+        {message && <div className="mb-4 text-red-500">{message}</div>}
+      </div>
+    </Layout>
+  );
+}
